5cc0e7cc5f10ebdcb8c42c869ddfbfecea9ea0d2
Sandeep <sandeepsk15892>
2023-05-19 17:15:03 +0530

    add data to firebase

--- lib/screen/new_item.dart
+++ lib/screen/new_item.dart
@@ -1,8 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 import '../data/categories.dart';
 import '../models/category.dart';
-import '../models/grocery_item.dart';
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
@@ -22,12 +24,20 @@ class _NewItemState extends State<NewItem> {
   void _saveItem() {
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
-      Navigator.of(context).pop(
-        GroceryItem(
-          id: DateTime.now().toString(),
-          name: _enteredName,
-          quantity: _enteredQuantity,
-          category: _selectedCategory,
+      final url = Uri.https(
+          'shoppingapp-5ee64-default-rtdb.asia-southeast1.firebasedatabase.app',
+          'shopping-list.json');
+      http.post(
+        url,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: json.encode(
+          {
+            'name': _enteredName,
+            'quantity': _enteredQuantity,
+            'category': _selectedCategory.title,
+          },
         ),
       );
     }
